9945cf7beacda646408287a9e9e4311a8fec22c6
Finnkauski <iwiivi@gmail.com>
2020-01-28 16:23:18 +0000

    add huemanity dependency from git

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,17 @@ use std::io::{stdin, stdout, Write};
 /// This function just wraps the flow of acquiring a midi input stream.
 /// It is based on the examples in the `midir` crate.
 pub fn acquire_midi_input() -> Result<(usize, MidiInput), Box<dyn Error>> {
-    let mut input = String::new();
-    let mut midi_in = MidiInput::new("midir reading input")?;
+    let mut input = String::new(); // needs a string buffer
+    let mut midi_in = MidiInput::new("drue")?;
     midi_in.ignore(Ignore::None);
 
     // Check which ports are available
-    println!("Availabe input ports:");
+    println!("Availabe midi inputs:");
     for input in 0..midi_in.port_count() {
         println!("{}: {}", input, midi_in.port_name(input).unwrap());
     }
 
-    print!("Please select input port: ");
+    print!("Please select midi input number: ");
     stdout().flush()?;
     stdin().read_line(&mut input)?;
     let in_port: usize = input.trim().parse()?;
